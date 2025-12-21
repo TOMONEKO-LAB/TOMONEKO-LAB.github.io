d19262d16d01ce9cc14f4385aa39579af22b6395
tomoneko-0831 <tomoneko0831@gmail.com>
2025-12-21 16:05:46 +0000

    Fix Static

--- src/app/api/compass/stages/route.ts
+++ src/app/api/compass/stages/route.ts
@@ -2,6 +2,8 @@ import fs from "fs"
 import path from "path"
 import { NextResponse } from "next/server"
 
+export const dynamic = "force-static"
+
 type StageInfo = {
 	member?: number
 	[k: string]: unknown
